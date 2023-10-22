5c1b2229a243bf8865b18e4107b979add404743b
UberRexu <110364272+UberRexu@users.noreply.github.com>
2023-10-23 05:18:00 +0700

    Prototype3.1

---
+++ ProjectC/Assets/Scripts/ObjectScript/Plank2.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plank2 : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Vector3 startPos;
+    private Vector3 currentPos;
+    private Vector3 endPos;
+    private bool movingForward = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(3,10);
+        startPos = transform.position;
+        endPos = startPos - new Vector3(0, 0, 22);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPos = transform.position;
+        if (movingForward)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+            if (currentPos.z <= endPos.z)
+            {
+                movingForward = false;
+            }
+        }
+        
+       else
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+            if (currentPos.z >= startPos.z)
+            {
+                movingForward = true;
+            }
+        }
+    }
+}
---
+++ ProjectC/Assets/Scripts/ObjectScript/Player.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator animator;
+    public bool isMoving;
+    public bool canMove = true;
+
+    [SerializeField] private TerrainGenerator terrainGenerator;
+    public float score;
+
+    [SerializeField] GameManager gameManager;
+
+    [SerializeField] private FollowPlayer Camera;
+
+    private bool isPause = false;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (canMove)
+        {
+            // Store the input direction.
+            Vector3 inputDirection = Vector3.zero;
+
+            if (Input.GetKeyDown(KeyCode.W) && !isMoving)
+            {
+                inputDirection = new Vector3(1, 0, 0);
+                Vector3 newPosition = transform.position + inputDirection;
+                if (CanMoveToPosition(newPosition, Vector3.right))
+                {
+                    MoveCharacter(inputDirection);
+                    Camera.PlayerStartedMovingForward();
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.A) && !isMoving)
+            {
+                inputDirection = new Vector3(0, 0, 1);
+                Vector3 newPosition = transform.position + inputDirection;
+                if (CanMoveToPosition(newPosition, Vector3.forward))
+                {
+                    MoveCharacter(inputDirection);
+                    Camera.PlayerStartedMovingLeftRight();
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.D) && !isMoving)
+            {
+                inputDirection = new Vector3(0, 0, -1);
+                Vector3 newPosition = transform.position + inputDirection;
+                if (CanMoveToPosition(newPosition, Vector3.back))
+                {
+                    MoveCharacter(inputDirection);
+                    Camera.PlayerStartedMovingLeftRight();
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.S) && !isMoving)
+            {
+                inputDirection = new Vector3(-1, 0, 0);
+                Vector3 newPosition = transform.position + inputDirection;
+                if (CanMoveToPosition(newPosition, Vector3.left))
+                {
+                    MoveCharacter(inputDirection);
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPause)
+            {
+                GameManager.Instance.UpdateGameState(GameState.Pause);
+                isPause = true;
+            }
+            else
+            {
+                GameManager.Instance.UpdateGameState(GameState.Unpause);
+                isPause = false;
+            }
+        }
+        gameManager.UpdateScore(UpdateScore());
+    }
+
+    private bool CanMoveToPosition(Vector3 newPosition, Vector3 whichway)
+    {
+        // Set the raycast origin at the player's feet.
+        Vector3 raycastOrigin = transform.position + Vector3.up * 0.1f;
+        // Cast raycasts in all directions to check for obstacles.
+        float raycastDistance = 1.0f;
+        RaycastHit hit;
+        if (Physics.Raycast(raycastOrigin, whichway, out hit, raycastDistance))
+        {
+            if (hit.collider.CompareTag("Obstacle"))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void MoveCharacter(Vector3 difference)
+    {
+        animator.SetTrigger("Move");
+        isMoving = true;
+        transform.position = (transform.position + difference);
+
+        if (terrainGenerator != null)
+        {
+            terrainGenerator.SpawnTerrains(false, false, transform.position);
+        }
+    }
+
+    public void FinishMove()
+    {
+        isMoving = false;
+        Camera.PlayerStandStill();
+    }
+
+    public float UpdateScore()
+    {
+        score = transform.position.x;
+        return score;
+    }
+}
